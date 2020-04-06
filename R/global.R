bfb2c8f648b7b3fcbffc0fd697594debab34f9c3
Pacome <pacome.pr@gmail.com>
2020-04-06 19:30:30 +0200

    moved function to prepro

--- R/global.R
+++ R/global.R
@@ -1,4 +1,4 @@
-#0. Importing packages ####
+# 0. Importing packages ####
 library(scater)
 library(scran)
 library(tibble)
@@ -14,14 +14,14 @@ library(GenomicRanges)
 library(splitstackshape)
 library(rlist)
 
-#Shiny
+# Shiny
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(plotly)
 library(shinyDirectoryInput)
 
-#Graphics
+# Graphics
 library(RColorBrewer)
 library(colorRamps)
 library(colourpicker)
@@ -29,38 +29,18 @@ library(kableExtra)
 library(knitr)
 library(viridis)
 library(ggplot2)
-  library(gplots)
+library(gplots)
 library(png)
 library(grid)
 library(gridExtra)
 library(DT)
 
-# global variables
-#options(scipen = 999)
+# global variables options(scipen = 999)
 jscode <- "shinyjs.closeWindow = function() { window.close(); }"
 
 gg_fill_hue <- function(n) {
-  hues = seq(15, 375, length = n + 1)
-  hcl(h = hues, l = 65, c = 100)[1:n]
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
 }
 
-choose_perplexity <- function(dataset){
-  perplexity=30
-  if(nrow(dataset) <= 200 ){
-    perplexity = 20
-  }
-  if(nrow(dataset) <= 250 ){
-    perplexity = 25
-  }
-  if (nrow(dataset) <= 150 ){
-    perplexity= 15
-  }
-  if (nrow(dataset) <= 100 ){
-    perplexity= 10
-  } 
-  if (nrow(dataset) <= 50 ) {
-    perplexity= 5
-  }
- perplexity
-}
 
