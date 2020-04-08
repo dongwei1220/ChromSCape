77e5b544c94c8c0126c12afe47a2c07b9b99d086
Pacome <pacome.pr@gmail.com>
2020-04-08 10:50:00 +0200

    added test data

--- tests/test_scChIP/test_ChromSCape_scChIP_synthetized_data.R
+++ tests/test_scChIP/test_ChromSCape_scChIP_synthetized_data.R
@@ -39,33 +39,65 @@ test_that("Step 3 : remove specific features ", {
   expect_equal(master$SelMatCov, assay(scExp))
 })
 
-scExp = normalize_scExp(scExp, "RPKM")
+scExp = normalize_scExp(scExp, "CPM")
 test_that("Step 4 : Normalize ", {
   load("tests/test_scChIP/filter_red_04.RData", master)
-  expect_equal(master$norm_mat, normcounts(scExp))
+  expect_equivalent(master$norm_mat, normcounts(scExp))
 })
 
-reference_annotation = read.table("annotation/hg38/Gencode_TSS_pc_lincRNA_antisense.bed",col.names = c("chr","start","end","Gene"))
-scExp = feature_annotation_scExp(scExp,reference_annotation)
+# reference_annotation = read.table("annotation/hg38/Gencode_TSS_pc_lincRNA_antisense.bed",col.names = c("chr","start","end","Gene"))
+# scExp = feature_annotation_scExp(scExp,reference_annotation)
+# 
+# test_that("Step 5 : feature annotation ", {
+#   load("tests/test_scChIP/Simulated_window_300_600_not_sparse_seed47_1600_1_95_uncorrected_annotFeat.RData",master)
+#   to_test = as.numeric(as.data.frame(rowData(scExp))$distance)
+#   original = as.numeric(makeGRangesFromDataFrame(master$annotFeat,
+#                                                  keep.extra.columns = T)$distance)
+#   original[original > 0] = original[original > 0] -2
+#   expect_equal(to_test,original)
+# })
 
-test_that("Step 5 : feature annotation ", {
-  load("tests/test_scChIP/Simulated_window_300_600_not_sparse_seed47_1600_1_95_uncorrected_annotFeat.RData",master)
-  
-  to_test = as.numeric(as.data.frame(rowData(scExp)$X)$distance)
-  original = as.numeric(makeGRangesFromDataFrame(master$annotFeat,
-                                                 keep.extra.columns = T)$distance)
-  original[original > 0] = original[original > 0] -2
-  expect_equal(to_test,original)
-})
+# test_that("Step 6 : batch correction ", {
+#   scExp = ChromSCape::filter_scExp(scExp)
+#   load("tests/test_scChIP/filter_red_02.RData", master)
+#   expect_equal(master$SelMatCov, assay(scExp))
+# })
 
-test_that("Step 6 : batch correction ", {
-  scExp = ChromSCape::filter_scExp(scExp)
-  load("tests/test_scChIP/filter_red_02.RData", master)
-  expect_equal(master$SelMatCov, assay(scExp))
-})
+scExp = reduce_dims_scExp(scExp)
 
 test_that("Step 7 : dimensionality reduction", {
-  scExp = (scExp)
-  load("tests/test_scChIP/filter_red_02.RData", master)
-  expect_equal(master$SelMatCov, assay(scExp))
+ 
+  load("tests/test_scChIP/Simulated_window_300_600_not_sparse_seed47_1600_1_95_uncorrected.RData", master)
+  
+  expect_equal(as.data.frame(master$pca),reducedDim(scExp,"PCA"))
 })
+
+scExp = correlation_and_hierarchical_clust_scExp(scExp)
+
+test_that("Step 8 : correlation & hiearchical clust", {
+  
+  load("tests/test_scChIP/Simulated_window_300_600_not_sparse_seed47_1600_1_95_uncorrected_99_1_cor_filtered.RData", master)
+  expect_equal(master$hc_cor$merge,scExp@metadata$hc_cor$merge)
+  expect_equal(master$hc_cor$height,scExp@metadata$hc_cor$height)
+  expect_equal(master$hc_cor$order,scExp@metadata$hc_cor$order)
+  expect_equal(master$hc_cor$labels,scExp@metadata$hc_cor$labels)
+  expect_equal(master$hc_cor$dist.method,scExp@metadata$hc_cor$dist.method)
+  expect_equal(master$hc_cor$call$method,scExp@metadata$hc_cor$call$method)
+  expect_equal(cor(t(master$pca)),reducedDim(scExp,"Cor") )
+  expect_equal(colnames(cor(t(master$pca))),colnames(reducedDim(scExp,"Cor")) )
+  expect_equal(rownames(cor(t(master$pca))),rownames(reducedDim(scExp,"Cor")) )
+})
+
+scExp. = filter_correlated_cell_scExp(scExp,random_iter = 500)
+
+test_that("Step 9 : correlation filterin clust", {
+  
+  load("tests/test_scChIP/Simulated_window_300_600_not_sparse_seed47_1600_1_95_uncorrected_99_1_cor_filtered.RData", master)
+  load("tests/test_scChIP/z.RData", master)
+  expect_equal(nrow(master$annot_sel),ncol(scExp.))
+  expect_equal(master$annot_sel$cell_id,colData(scExp.)$cell_id)
+  table(master$annot_sel$sample_id)
+  table(colData(scExp.)$sample_id)
+  
+})
+
