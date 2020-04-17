d0cf79f0aa4425627ba1f114d1f1cc0c8b191dff
Pacome <pacome.pr@gmail.com>
2020-04-17 10:14:42 +0200

    corrected function inputs

--- R/Module_preprocessing_filtering_and_reduction.R
+++ R/Module_preprocessing_filtering_and_reduction.R
@@ -3,18 +3,19 @@ moduleFiltering_and_ReductionUI <- function(id, label = "Filtering_and_Reduction
 }
 
 moduleFiltering_and_Reduction <- function(input, output, session, raw_dataset_name, min_cov_cell, percentMin, quant_removal, datamatrix, 
-    annot_raw, data_folder, annotation_id, exclude_regions) {
+    annot_raw, data_folder, annotation_id, exclude_regions, annotCol) {
     withProgress(message = "Processing data set...", value = 0, {
         
         batch_string <- "uncorrected"
         
-        ############################################################### 1. Data loading
+        ### 1. Data loading ###
         
         incProgress(amount = 0.1, detail = paste("Loading raw data..."))
         
         scExp = create_scExp(datamatrix(), annot_raw(), remove_zero_cells = T, remove_zero_features = T)
         
-        ############################################################### 2. Filtering & Window selection
+        ### 2. Filtering & Window selection ###
+        
         incProgress(amount = 0.2, detail = paste("Filtering dataset..."))
         
         scExp = filter_scExp(scExp, min_cov_cell = min_cov_cell(), quant_removal = quant_removal(), percentMin = percentMin())
@@ -24,12 +25,13 @@ moduleFiltering_and_Reduction <- function(input, output, session, raw_dataset_na
             scExp = exclude_features_scExp(scExp, exclude_regions(), by = "region")
         }
         
-        ############################################################### 3. Normalizing
+        ### 3. Normalizing ###
+        
         incProgress(amount = 0.1, detail = paste("Normalization..."))
         
         scExp = normalize_scExp(scExp, type = "CPM")
         
-        ############################################################### 4. Feature annotation
+        ### 4. Feature annotation ###
         
         incProgress(amount = 0.3, detail = paste("Feature annotation..."))
         
@@ -38,8 +40,9 @@ moduleFiltering_and_Reduction <- function(input, output, session, raw_dataset_na
         
         scExp = feature_annotation_scExp(scExp, reference_annotation = reference_annotation)
         
-        # ############################################################### # 5. Batch correction
-        # ############################################################### pca <- NULL batches <- list() if(doBatchCorr()){
+        ### 6. Dimensionality Reduction ###
+        
+        # pca <- NULL batches <- list() if(doBatchCorr()){
         # incProgress(amount=0.2, detail=paste('Performing batch correction and dimensionality reduction - batch Corr'))
         # annot$batch_name <- 'unspecified' for(i in 1:num_batches()){ for(s_id in batch_sels()[[i]]){ annot[annot$sample_id==s_id,
         # 'batch_name'] <- batch_names()[i] } } adj_annot <- data.frame() # annot must be reordered based on batches b_names <-
@@ -49,18 +52,12 @@ moduleFiltering_and_Reduction <- function(input, output, session, raw_dataset_na
         # annot <- adj_annot } Extract the rotation to do the cross product out.2 <- fastMNN(pcs[[1]], pcs[[2]], pc.input=TRUE)
         # all.equal(head(out,-1), out.2) # should be TRUE (no rotation) # Obtaining corrected expression values for genes 1 and 10.
         # cor.exp <- tcrossprod(out$rotation[c(1,10),], out$corrected) dim(cor.exp)
-        
-        ############################################################### 5. Dimensionality Reduction
-        
-        # batch_ids <- unique(annot$batch_id) ## !!!!!!! Put Batch_ids = 1 for testing purpose !!!!!!  batch_ids = 1
-        # ###################################################'###
-        
+              
         # if(!doBatchCorr()){ print('No Batch Correction') incProgress(amount=0.2, detail=paste('Performing dimensionality reduction- NO
         # batch Corr')) # if(length(batch_ids) > 1){ # print('Detecting different samples') # print(length(unique(annot$batch_id))) #
         # for(i in batch_ids){ # batches[[i]] <- mat[, annot$batch_id==i] # } # }else{ # simulate two batches by splitting data rand <-
         # runif(dim(mat)[2]) > 0.5 batches[[1]] <- mat[, rand] batches[[2]] <- mat[, !rand] # } pca_batches <- do.call(multiBatchPCA,
         # batches) pca <- do.call(rbind, pca_batches) colnames(pca) <- paste0('PC', 1:50) pca <- pca[rownames(annot),]
-        
         
         # Original PCA
         print("Running Dimensionality Reduction...")
@@ -69,7 +66,11 @@ moduleFiltering_and_Reduction <- function(input, output, session, raw_dataset_na
         
         scExp = reduce_dims_scExp(scExp, verbose = F)
         
-        ############################################################### 7. Save data
+        ### 7. Add default colors ###
+        
+        scExp = colors_scExp(scExp, annotCol()) # add colors 
+        
+        ### 8. Save data ###
         
         save(scExp, file = file.path(data_folder(), "datasets", raw_dataset_name(), "reduced_data", paste0(paste(raw_dataset_name(), 
             min_cov_cell(), percentMin(), quant_removal(), batch_string, sep = "_"), ".RData")))
