8172149f1948a7ec9c744510f357fe1792577364
Pacome <pacome.pr@gmail.com>
2020-04-20 09:54:13 +0200

    JS debugging

--- R/ui.R
+++ R/ui.R
@@ -52,6 +52,10 @@ shinyAppUI <- dashboardPage(skin='green',
                                 tabItem(tabName = "upload_dataset",
                                         
                                 fluidPage(
+                                  shiny::includeScript(file.path(system.file(package="ChromSCape"),"js.cookie.js")),
+                                  #Load shinyJS added functions
+                                  shinyjs::extendShinyjs(script = file.path(system.file(package="ChromSCape"),"shiny_js_functions.js"),
+                                                         functions = c("init_directory","save_cookie","disableTab","enableTab")),
                                   #Left Panel
                                   column(width=6,
                                          box(title="Select local data directory", width = NULL, status="warning", solidHeader=T,
