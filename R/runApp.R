f3796e9ffdde2ddb251e58ab17a0f1f1f18b4bf9
Pacome Prompsy <36704054+Pacomito@users.noreply.github.com>
2020-04-23 12:04:20 +0200

    Sourcing module at start of app

--- R/runApp.R
+++ R/runApp.R
@@ -8,5 +8,6 @@
 #'  
 launchApp <- function() {
   # shinyApp(ui = shinyAppUI, server = shinyAppServer)
+  source(file.path(system.file(package="ChromSCape"),"R","Module_preprocessing_filtering_and_reduction.R"))
   shiny::runApp(system.file(package="ChromSCape"))
 }
