674994ba74250548b9b1fc9528622e8e2be091ae
Pacome <pacome.pr@gmail.com>
2020-04-23 12:31:43 +0200

    module

--- R/runApp.R
+++ R/runApp.R
@@ -8,6 +8,6 @@
 #'  
 launchApp <- function() {
   # shinyApp(ui = shinyAppUI, server = shinyAppServer)
-  source(file.path(system.file(package="ChromSCape"),"R","Module_preprocessing_filtering_and_reduction.R"))
+  source(file.path(system.file(package="ChromSCape"),"Module_preprocessing_filtering_and_reduction.R"))
   shiny::runApp(system.file(package="ChromSCape"))
 }
