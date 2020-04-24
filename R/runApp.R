076c7f611fb17b679b3baba67b7603fe56b82889
Pacome <pacome.pr@gmail.com>
2020-04-24 11:07:40 +0200

    module name

--- R/runApp.R
+++ R/runApp.R
@@ -6,7 +6,8 @@
 #' 
 #' @import shiny
 #'  
-launchApp <- function() {
+launchApp <- function(){
+  
   # shinyApp(ui = shinyAppUI, server = shinyAppServer)
   source(file.path(system.file(package="ChromSCape"),"Module_preprocessing_filtering_and_reduction.R"))
   shiny::runApp(system.file(package="ChromSCape"))
